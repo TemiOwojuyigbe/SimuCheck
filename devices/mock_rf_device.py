d39405990ac1858ba27aa17bbe8841ce8a6674cd
TemiOwojuyigbe <114966961+TemiOwojuyigbe@users.noreply.github.com>
2025-04-14 23:02:16 -0400

    updated

---
+++ devices/mock_rf_device.py
@@ -0,0 +1,26 @@
+import random # to simulate variablity in signal strength
+
+class RFDevices: # acts as a fake or simualted device
+    def __init__(self):
+        self.powered_on = False
+        self.signal_strenth = 0
+
+    #simulates powering on the device - once powered on it randomly sets the signal strength between 60 and 100   
+    def power_on(self):
+        self.powered_on = True 
+        self.signal_strength = random.randint(60, 100)
+    
+    #siumaltes a diagnostic test - checks if the device is on. if not it returns a failue message
+    def run_diagostics(self):
+        if not self.powered_on:
+            return {"status": "Fail", "reason": "device not powered on"}
+        #simualte test resultes
+        return {
+            "status": "PASS" if self.signal_strength > 70 else "FAIL",
+            "signal_strength": self.signal_strength
+        }
+    
+    #simulates a reboot - resets device to its default state
+    def reset(self):
+        self.powered_on = False
+        self.signal_strength = 0
